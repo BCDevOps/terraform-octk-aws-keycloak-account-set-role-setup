c3b48aafbeb025fa87ff8e55bf76a4701426d3d5
Shea Phillips <shea.phillips@gmail.com>
2020-11-24 16:39:53 -0800

    Initial version.

--- main.tf
+++ main.tf
@@ -1,1 +1,33 @@
-//put your awesome Terraform code here.
+terraform {
+	required_providers {
+		keycloak = {
+			source = "mrparkers/keycloak"
+			version = "2.0.0"
+		}
+		aws = {
+			source = "hashicorp/aws"
+			version = "3.11.0"
+		}
+	}
+}
+
+provider "keycloak" {
+	client_id = var.kc_terraform_auth_client_id
+	client_secret = var.kc_terraform_auth_client_secret
+	realm = var.kc_realm
+	url = var.kc_base_url
+}
+
+locals {
+	workload_account_roles = {
+		"BCGOV_WORKLOAD_admin_${var.kc_realm}": "arn:aws:iam::aws:policy/AdministratorAccess"
+		"BCGOV_WORKLOAD_readonly_${var.kc_realm}": "arn:aws:iam::aws:policy/job-function/ViewOnlyAccess"
+	}
+
+	trusted_login_sources = var.custom_login_url == null ? [
+		"https://signin.aws.amazon.com/saml"] : [
+		"https://signin.aws.amazon.com/saml",
+		var.custom_login_url]
+	idp_name = "BCGovKeyCloak-${var.kc_realm}"
+}
+
--- variables.tf
+++ variables.tf
@@ -1,1 +1,35 @@
-//module variables should be defined and documented here.
+variable "kc_realm" {
+	description = "KeyCloak realm where terraform client has been created and where users/groups to be created/manipulated exist."
+	type        = string
+}
+
+variable "kc_terraform_auth_client_id" {
+	description = "Client ID of client that terraform will authenticate against in order to do its work."
+	type        = string
+}
+
+variable "kc_terraform_auth_client_secret" {
+	description = "Client secret used by Terraform KeyCloak provider authenticate against KeyCloak."
+	type = string
+}
+
+variable "kc_iam_auth_client_id" {
+	description = "Client ID of client where KC roles corresponding to AWS roles will be created."
+	type        = string
+}
+
+variable "kc_base_url" {
+	description = "Base URL of KeyCloak instance to interact with."
+	type = string
+}
+
+variable "custom_login_url" {
+	description = "URL of custom login page/app."
+	type = string
+	default = null
+}
+
+variable "project_accounts" {
+	type = map(any)
+	description = "A map of the project accounts (with structure matching output of aws_organizations_account) for which we will be creating roles and IDP resources, keyed by the name of the envrionment."
+}
