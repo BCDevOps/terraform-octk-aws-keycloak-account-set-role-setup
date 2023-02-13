d63aedad70d6c88b86b7f49ba43c9dab29b205af
Warren Uniewski <warren@autimo.com>
2023-02-13 15:12:12 -0800

    add gha workflows

--- main.tf
+++ main.tf
@@ -1,40 +1,40 @@
 provider "keycloak" {
-	client_id = var.kc_terraform_auth_client_id
-	client_secret = var.kc_terraform_auth_client_secret
-	realm = var.kc_realm
-	url = var.kc_base_url
+  client_id     = var.kc_terraform_auth_client_id
+  client_secret = var.kc_terraform_auth_client_secret
+  realm         = var.kc_realm
+  url           = var.kc_base_url
 }
 
 locals {
-	// massage new "role config" variable structure into original structure
-	workload_account_roles = { for role_spec in var.workload_account_role_config : role_spec.aws_role_name => role_spec.aws_policy_arn }
+  // massage new "role config" variable structure into original structure
+  workload_account_roles = { for role_spec in var.workload_account_role_config : role_spec.aws_role_name => role_spec.aws_policy_arn }
 
-	trusted_login_sources = var.custom_login_url == null ? [
-		"https://signin.aws.amazon.com/saml"] : [
-		"https://signin.aws.amazon.com/saml",
-		var.custom_login_url]
-	idp_name = "BCGovKeyCloak-${var.kc_realm}"
+  trusted_login_sources = var.custom_login_url == null ? [
+    "https://signin.aws.amazon.com/saml"] : [
+    "https://signin.aws.amazon.com/saml",
+  var.custom_login_url]
+  idp_name = "BCGovKeyCloak-${var.kc_realm}"
 }
 
 data "keycloak_realm" "realm" {
-	realm   = var.kc_realm
+  realm = var.kc_realm
 }
 
 data "keycloak_group" "tenant_group" {
-	realm_id = data.keycloak_realm.realm.id
-	name     = "Project Team Groups"
+  realm_id = data.keycloak_realm.realm.id
+  name     = "Project Team Groups"
 }
 
 resource "keycloak_group" "project_group" {
-	realm_id  = data.keycloak_realm.realm.id
-	parent_id = data.keycloak_group.tenant_group.id
-	name = "${var.project_spec.identifier} (${var.project_spec.name})"
+  realm_id  = data.keycloak_realm.realm.id
+  parent_id = data.keycloak_group.tenant_group.id
+  name      = "${var.project_spec.identifier} (${var.project_spec.name})"
 }
 
 resource "keycloak_group" "role_groups" {
-	for_each = toset([ for role_spec in var.workload_account_role_config : role_spec.keycloak_group_name ])
+  for_each = toset([for role_spec in var.workload_account_role_config : role_spec.keycloak_group_name])
 
-	realm_id  = data.keycloak_realm.realm.id
-	parent_id = keycloak_group.project_group.id
-	name      = each.key
+  realm_id  = data.keycloak_realm.realm.id
+  parent_id = keycloak_group.project_group.id
+  name      = each.key
 }
--- variables.tf
+++ variables.tf
@@ -1,60 +1,60 @@
 variable "kc_realm" {
-	description = "KeyCloak realm where terraform client has been created and where users/groups to be created/manipulated exist."
-	type = string
+  description = "KeyCloak realm where terraform client has been created and where users/groups to be created/manipulated exist."
+  type        = string
 }
 
 variable "kc_terraform_auth_client_id" {
-	description = "Client ID of client that terraform will authenticate against in order to do its work."
-	type = string
+  description = "Client ID of client that terraform will authenticate against in order to do its work."
+  type        = string
 }
 
 variable "kc_terraform_auth_client_secret" {
-	description = "Client secret used by Terraform KeyCloak provider authenticate against KeyCloak."
-	type = string
+  description = "Client secret used by Terraform KeyCloak provider authenticate against KeyCloak."
+  type        = string
 }
 
 variable "kc_iam_auth_client_id" {
-	description = "Client ID of client where KC roles corresponding to AWS roles will be created."
-	type = string
+  description = "Client ID of client where KC roles corresponding to AWS roles will be created."
+  type        = string
 }
 
 variable "kc_base_url" {
-	description = "Base URL of KeyCloak instance to interact with."
-	type = string
+  description = "Base URL of KeyCloak instance to interact with."
+  type        = string
 }
 
 variable "custom_login_url" {
-	description = "URL of custom login page/app."
-	type = string
-	default = null
+  description = "URL of custom login page/app."
+  type        = string
+  default     = null
 }
 
 variable "project_accounts" {
-	type = map(any)
-	description = "A map of the project accounts (with structure matching output of aws_organizations_account) for which we will be creating roles and IDP resources, keyed by the name of the envrionment."
+  type        = map(any)
+  description = "A map of the project accounts (with structure matching output of aws_organizations_account) for which we will be creating roles and IDP resources, keyed by the name of the envrionment."
 }
 
 variable "workload_account_role_config" {
-	description = "A mapping of role names to be created to (existing) policy arns."
-//	todo provide a more complete schema for this
-	type = list(map(string))
+  description = "A mapping of role names to be created to (existing) policy arns."
+  //	todo provide a more complete schema for this
+  type = list(map(string))
 }
 
 variable "tenancy_root_group_name" {
-	type = string
-	default = "Project Team Groups"
+  type    = string
+  default = "Project Team Groups"
 }
 
 variable "project_spec" {
-	description = "List of projects/(accounts) that product teams' workloads run within."
-	type = object({
-		identifier = string
-		name = string
-		tags = map(string)
-		accounts = list(object({
-			name = string
-			environment = string
-		}))
-	})
+  description = "List of projects/(accounts) that product teams' workloads run within."
+  type = object({
+    identifier = string
+    name       = string
+    tags       = map(string)
+    accounts = list(object({
+      name        = string
+      environment = string
+    }))
+  })
 }
 
